fa6db37a2397a27ba5007630119068883eaef490
MarioMario7 <116391314+MarioMario7@users.noreply.github.com>
2026-05-01 17:32:46 +0300

    old changes added

--- include/nnue_features.hpp
+++ include/nnue_features.hpp
@@ -1,32 +1,25 @@
-#ifndef CHESSENGINE_NNUE_FEATURES_HPP
-#define CHESSENGINE_NNUE_FEATURES_HPP
+#pragma once
 
-#include "../chess-library-master/include/chess.hpp"
 #include <vector>
+#include "chess.hpp"
 
-namespace chessengine
-{
+namespace chessengine {
+    using namespace chess;
+
     struct HalfKP_Feature
     {
         int king_square;
         int piece_square;
-        chess::PieceType piece_type;
-        chess::Color piece_color;
-        chess::Color side;
+        PieceType piece_type;
+        Color piece_color;
+        Color side;
     };
 
-    int piece_type_to_index(chess::PieceType piece_type);
-    int color_to_index(chess::Color color);
+    std::vector<HalfKP_Feature> get_active_features_halfkp(const Board& board, Color side);
 
-    void add_features(const chess::Board& board, std::vector<HalfKP_Feature>& features, chess::PieceType piece_type, chess::Color piece_color, chess::Color side, int king_square);
-
-    void print_halfkp_features(const std::vector<HalfKP_Feature>& features);
-
-    std::vector<HalfKP_Feature> get_active_features_halfkp(const chess::Board& board, chess::Color side);
+    std::vector<int> get_active_feature_indices_halfkp(const Board& board, Color side);
 
     int halfkp_index(const HalfKP_Feature& feature);
 
-    std::vector<int> get_active_feature_indices_halfkp(const chess::Board& board, chess::Color side);
+    void print_halfkp_features(const std::vector<HalfKP_Feature>& features);
 }
-
-#endif
